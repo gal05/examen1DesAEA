271183e91b22e783d069604872a7e90552eb07b5
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2019-03-18 09:01:10 -0500

    borrar sino funciona

--- Evaluacion/FrmIngreso.Designer.cs
+++ Evaluacion/FrmIngreso.Designer.cs
@@ -128,7 +128,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(452, 167);
+            this.ClientSize = new System.Drawing.Size(799, 266);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnSalir);
--- Evaluacion/FrmIngreso.cs
+++ Evaluacion/FrmIngreso.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Evaluacion.Extras;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,11 +22,36 @@ namespace Evaluacion
         private void FrmIngreso_Load(object sender, EventArgs e)
         {
             timer1.Enabled = true;
+            ListaAnios();//:v
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             label3.Text = DateTime.Now.ToLongTimeString();
         }
+
+
+
+
+        //GEGE
+        public void ListaAnios()
+        {
+            using (SqlCommand cmd = new SqlCommand("Usp_listaAnios", Conexion.conexion()))
+            {
+                using (SqlDataAdapter adapter = new SqlDataAdapter())
+                {
+                    adapter.SelectCommand = cmd;
+                    adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    using (DataSet ds = new DataSet())
+                    {
+                        adapter.Fill(ds, "ListaAnios");
+                        cbxAnio.DataSource = ds.Tables["ListaAnios"];
+                        cbxAnio.DisplayMember = "anios";
+                        cbxAnio.ValueMember = "anios";
+                    }
+                }
+            }
+        }
+        //GEGE :D
     }
 }
