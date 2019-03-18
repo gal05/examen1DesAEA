e30eef9901873a7dd91b2d78b7d0b93048de1ae8
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2019-03-18 08:58:29 -0500

    conexion done

---
+++ Evaluacion/Extras/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion.Extras
+{
+    public static class Conexion
+    {
+        public static SqlConnection conexion()
+        {
+            SqlConnection cn = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["GUERRA"].ConnectionString);
+            return cn;
+        }
+    }
+}
